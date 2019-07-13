96550ff9407dad869cd5d1f1ca2fca2622b39c3f
Amit Kumar <42837133+akumar1999@users.noreply.github.com>
2019-07-13 00:40:49 -0400

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -30,15 +30,15 @@ class Projects extends Component {
       )
     } else if(this.state.activeTab === 1) {
       return (
-        <div><h1>This is Angular</h1></div>
+        <div><h1>This is Python</h1></div>
       )
     } else if(this.state.activeTab === 2) {
       return (
-        <div><h1>This is VueJS</h1></div>
+        <div><h1>This is ReactNative</h1></div>
       )
     } else if(this.state.activeTab === 3) {
       return (
-        <div><h1>This is MongoDB</h1></div>
+        <div><h1>This is Machine Learning</h1></div>
       )
     }
   }
